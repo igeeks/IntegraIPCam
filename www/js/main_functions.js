ac211d3d11bc8f873c7bde067be3ee058dd44ae9
evostrov <vostrov.e@gmail.com>
2012-11-13 19:32:16 +0400

    Capture video on index.html

--- www/js/main_functions.js
+++ www/js/main_functions.js
@@ -1,3 +1,68 @@
+
+// ========================= Video =========================
+var widthRes = 0;
+var heightRes = 0;
+var resizeBtn = true;
+
+function fullScrin(){
+    $(window).scrollTop(0); 
+    var pObj = $('#resizeObl');
+    var pObjEmbd = $('#embed1231');
+    widthRes = pObjEmbd.width();
+    heightRes = pObjEmbd.height();
+    pObj.css("position", "absolute");
+    pObj.css("z-index", "8999");
+    pObj.width(embed1231.picwidth);
+    pObj.height(embed1231.picheight);
+    pObjEmbd.width(embed1231.picwidth);
+    pObjEmbd.height(embed1231.picheight);
+}
+function miniScrin(){
+    var pObj = $('#resizeObl');
+    var pObjEmbd = $('#embed1231');
+    pObj.css({left:'', top:''});
+    pObj.width(widthRes);
+    pObj.height(heightRes);
+    pObjEmbd.width(widthRes);
+    pObjEmbd.height(heightRes);
+    pObj.css("position", "");
+    pObj.css("z-index", "");
+}
+function ResizeEmbed(){
+    if (resizeBtn)
+        fullScrin();
+    else
+        miniScrin();
+    resizeBtn = !resizeBtn;
+    embed1231.Resize();
+}
+
+function ScreenEmbed1(){
+    $(window).scrollTop(0); 
+    var pObjEmbd = $('#embed1231');
+    pObjEmbd.width(embed1231.picwidth*2);
+    pObjEmbd.height(embed1231.picheight*2);
+}
+function ScreenEmbed2(){
+    $(window).scrollTop(0); 
+    var pObjEmbd = $('#embed1231');
+    pObjEmbd.width(embed1231.picwidth);
+    pObjEmbd.height(embed1231.picheight);
+}
+function ScreenEmbed3(){
+    $(window).scrollTop(0); 
+    var pObjEmbd = $('#embed1231');
+    pObjEmbd.width(embed1231.picwidth/2);
+    pObjEmbd.height(embed1231.picheight/2);
+}
+function ScreenEmbed4(){
+    $(window).scrollTop(0); 
+    var pObjEmbd = $('#embed1231');
+    pObjEmbd.width(320);
+    pObjEmbd.height(240);
+}
+
+// ========================= Page logic =========================
 $(document).ready(function() {
     // Анимация инициализации
     show_loader('init', 'Загрузка');
